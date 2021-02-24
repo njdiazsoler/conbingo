a3c315169e85c2682bb563ca463161fd950f3e06
njdiazsoler <njdiaz90@gmail.com>
2021-02-24 16:43:39 -0300

    added logic to App test suite

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,22 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 
-test('Generate button is present and working', () => {
+test('Page content is present', async () => {
   render(<App />);
-  const buttonElement = screen.getByRole(/GenerateButton/i);
+  const buttonElement = screen.getByRole(/generate-button/i);
+  const cardContainer = screen.getByTestId(/card-container/i)
+  const headerElement = screen.getByTestId(/page-header/i)
   expect(buttonElement).toBeInTheDocument();
-  fireEvent.click(buttonElement);
-  const textElement = screen.getByText('Button clicked!')
-  expect(textElement).toBeInTheDocument();
+  expect(cardContainer).toBeInTheDocument();
+  expect(headerElement).toBeInTheDocument();
+});
+
+test('Generate button is present and working', async () => {
+  render(<App />);
+  const generateButton = screen.getByRole(/generate-button/i);
+  const downloadButton = screen.getByRole(/download-button/i);
+  expect(generateButton).toBeInTheDocument();
+  expect(downloadButton).toBeDisabled();
+  fireEvent.click(generateButton);
+  expect(downloadButton).toBeEnabled();
 });
