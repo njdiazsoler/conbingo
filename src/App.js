35bac4fae53217867f879bf686d0fd7e998a3794
njdiazsoler <njdiaz90@gmail.com>
2021-02-25 18:15:38 -0300

    added routing logic to app, moved component logic to separate file

--- src/App.js
+++ src/App.js
@@ -1,78 +1,51 @@
-import { useState } from "react";
-import "./App.css";
-import image from "./assets/img/Supporter-Blizzard-Entertainment-Logo.png";
-import data from "./data.json";
-import * as htmlToImage from "html-to-image";
-import { saveAs } from "file-saver";
+import React from "react";
 import {
-  Button,
-  Image as BootstrapImage,
-} from "react-bootstrap";
-import BingoCard from "./components/BingoCard";
-import parsingUtils from "./utils/parsing";
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link,
+  // useRouteMatch,
+  // useParams,
+} from "react-router-dom";
+import { Fade } from 'react-bootstrap';
+import Layout from "./views/Layout";
+import Home from './views/Home';
+import BingoCard from './views/BingoCard';
+import data from "./data.json";
+import "./App.css";
 
-const App = () => {
-  const defaultItems = new Array(25).fill(false);
-  const [items, setItems] = useState(defaultItems);
-
-  const handleGenerateClick = () => {
-    // const newTable = new Array(25).fill().map(() => Math.floor(Math.random() * 10))
-    const newTable = parsingUtils.shuffle(
-      new Array(25)
-        .fill()
-        .map((el, ix) => data.blizzard[ix])
-    );
-    setItems(newTable);
-  };
-
-  const createDownloadFile = async () => {
-    const node = document.getElementById("main-app");
-
-    try {
-      const imgBlob = await htmlToImage.toBlob(node, {
-        backgroundColor: "#282c34",
-        pixelRatio: 1,
-      });
-      saveAs(imgBlob, "conbingo.png");
-    } catch (error) {
-      console.error("oops, something went wrong!", error);
-    }
-  };
-
+const App = props => {
   return (
-    <div className="App">
-      <div id="main-app">
-        <header className="App-header" data-testid="page-header">
-          <div className="flex">
-            <BootstrapImage
-              className="custom-img"
-              fluid
-              src={image}
-              role="header-image"
-            />
-          </div>
-        </header>
-        <div className="container" data-testid="card-container">
-          <BingoCard items={items} />
+    <Layout>
+      <Router>
+        <div>
+          <Switch>
+            <FadingRoute path="/nintendo" component={BingoCard} />
+            <FadingRoute path="/microsoft" component={BingoCard} />
+            <FadingRoute path="/blizzard" component={BingoCard} />
+            <FadingRoute path="/playstation" component={BingoCard} />
+            {/* <Route path="/ea" component={About} />
+          <Route path="/square" component={About} /> */}
+            {/* <FadingRoute path="/custom" component={BingoCard} /> */}
+            <FadingRoute path="/" component={Home} />
+          </Switch>
         </div>
-      </div>
-      <div>
-        <Button
-          role="generate-button"
-          onClick={handleGenerateClick}
-        >
-          Generate!
-        </Button>
-        <Button
-          disabled={items.every((item) => !item)}
-          role="download-button"
-          onClick={() => createDownloadFile()}
-        >
-          Download!
-        </Button>
-      </div>
-    </div>
+      </Router>
+    </Layout>
   );
-};
+}
+
+const FadingRoute = ({ component: Component, path, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={routeProps => (
+        <Fade appear={true}>
+          <Component {...routeProps} data={data[path.substr(1)]} />
+        </Fade>
+      )}
+    />
+  );
+}
 
 export default App;
